f8fda193a23f4739ef8e679057ea14eac051dfde
Roberto Groza <robertogroza@gmail.com>
2025-09-15 18:15:33 +0000

    Basic PID goal pos controller working on real robot

--- src/distance_controller.cpp
+++ src/distance_controller.cpp
@@ -18,21 +18,14 @@ using Odometry = nav_msgs::msg::Odometry;
 using Twist = geometry_msgs::msg::Twist;
 using Float32MultiArray = std_msgs::msg::Float32MultiArray;
 
-// PID gains
-constexpr float kp = 4.0f;
-constexpr float ki = 1.0f;
-constexpr float kd = 1.0f;
-
 constexpr float deriv_lpf_alpha = 0.2f;
 constexpr float integ_limit = 0.5f;
-
-constexpr float max_twist_magnitude = 0.2f;
 constexpr float min_pos_error = 0.01f;
 
 class DistanceController : public rclcpp::Node {
 public:
   DistanceController(int scene_number) : Node("distance_controller"), scene_number_(scene_number) {
-    odom_subscriber_ = this->create_subscription<Odometry>("rosbot_xl_base_controller/odom", 10,
+    odom_subscriber_ = this->create_subscription<Odometry>("odometry/filtered", 10,
                                                            std::bind(&DistanceController::odom_callback, this, _1));
 
     rclcpp::QoS qos_profile(rclcpp::KeepLast(10));
@@ -52,28 +45,38 @@ public:
     integral_.setZero();
     twist_.setZero();
 
-    Kp_ << kp, kp;
-    Ki_ << ki, ki;
-    Kd_ << kd, kd;
-
     // Assigning waypoints based on scene number
     switch (scene_number_) {
     case 1: // Simulation
+      kp_ = 3.0f;
+      ki_ = 1.0f;
+      kd_ = 0.7f;
+      kp_ang_ = 0.0f;
+      max_twist_mag_ = 0.9f;
       waypoints_ = {{0.0f, 1.0f},   {0.0f, -1.0f}, {0.0f, -1.0f}, {0.0f, 1.0f}, {1.0f, 1.0f},
                     {-1.0f, -1.0f}, {1.0f, -1.0f}, {-1.0f, 1.0f}, {1.0f, 0.0f}, {-1.0f, 0.0f}};
       break;
     case 2: // CyberWorld
-      waypoints_ = {{0.92f, 0.0f}, {0.0f, -0.7f}, {0.0f, 0.7f}, {-0.92f, 0.0f}};
+      kp_ = 3.0f;
+      ki_ = 1.0f;
+      kd_ = 0.5f;
+      kp_ang_ = 2.0f;
+      max_twist_mag_ = 0.3f;
+      waypoints_ = {{0.90f, 0.0f}, {0.0f, -0.5f}, {0.0f, 0.5f}, {-0.90f, 0.0f}};
       break;
     default:
       RCLCPP_ERROR(this->get_logger(), "Invalid Scene Number: %d", scene_number_);
     }
 
+    Kp_ << kp_, kp_;
+    Ki_ << ki_, ki_;
+    Kd_ << kd_, kd_;
+
     waypoint_idx_ = 0;
 
     cmd_vel_msg_ = Twist();
     data_msg_ = Float32MultiArray();
-    data_msg_.data.resize(2);
+    data_msg_.data.resize(5);
   }
 
 private:
@@ -85,6 +88,9 @@ private:
   Eigen::Vector2f goal_pos_, odom_pos_, odom_vel_;
   Eigen::Vector2f error_, error_dot_, integral_, twist_;
   Eigen::Vector2f Kp_, Ki_, Kd_;
+  Eigen::Matrix2f Rot_;
+  float kp_, ki_, kd_, kp_ang_, max_twist_mag_;
+  float odom_yaw_, odom_init_yaw_;
   std::vector<Eigen::Vector2f> waypoints_;
   size_t waypoint_idx_;
   Twist cmd_vel_msg_;
@@ -100,9 +106,16 @@ private:
     odom_pos_(1) = static_cast<float>(msg->pose.pose.position.y);
     odom_vel_(0) = static_cast<float>(msg->twist.twist.linear.x);
     odom_vel_(1) = static_cast<float>(msg->twist.twist.linear.y);
+
+    const auto q = msg->pose.pose.orientation;
+    odom_yaw_ = static_cast<float>(std::atan2(2.0f * (q.w * q.z + q.x * q.y), 1.0f - 2.0f * (q.y * q.y + q.z * q.z)));
+
     if (!have_init_pos_) {
       goal_pos_ = odom_pos_;
+      odom_init_yaw_ = odom_yaw_;
+      Rot_ = Eigen::Rotation2D<float>(odom_init_yaw_).toRotationMatrix();
       have_init_pos_ = true;
+      RCLCPP_INFO(this->get_logger(), "Initial odom pos:\t\t(%.2f, %.2f), yaw: %.2f", odom_pos_(0), odom_pos_(1), odom_init_yaw_);
     }
   }
 
@@ -115,14 +128,14 @@ private:
       rclcpp::sleep_for(2000ms);
 
       if (waypoint_idx_ < waypoints_.size()) {
-        goal_pos_ += waypoints_[waypoint_idx_];
+        goal_pos_ += Rot_ * waypoints_[waypoint_idx_];
       } else {
         rclcpp::shutdown();
         return;
       }
 
-      RCLCPP_INFO(this->get_logger(), "Strafing to waypoint %zu:\t(%.2f, %.2f)", waypoint_idx_ + 1,
-                  waypoints_[waypoint_idx_][0], waypoints_[waypoint_idx_][1]);
+      RCLCPP_INFO(this->get_logger(), "Strafing to waypoint %zu:\t(%.2f, %.2f) --> (%.2f, %.2f)", waypoint_idx_ + 1,
+                  waypoints_[waypoint_idx_][0], waypoints_[waypoint_idx_][1], goal_pos_(0), goal_pos_(1));
 
       waypoint_idx_++;
     }
@@ -135,8 +148,8 @@ private:
     last_time_ = now;
     have_time_ = true;
 
-    // Position error
-    error_ = goal_pos_ - odom_pos_;
+    // Position error in the body frame
+    error_ = Rot_.transpose() * (goal_pos_ - odom_pos_);
 
     // Derivative on measurement
     error_dot_ = -deriv_lpf_alpha * odom_vel_ + (1.0f - deriv_lpf_alpha) * error_dot_;
@@ -156,8 +169,8 @@ private:
 
     // Saturate twist magnitude
     float twist_norm = twist_.norm();
-    if (twist_norm > max_twist_magnitude)
-      twist_ *= (max_twist_magnitude / twist_norm);
+    if (twist_norm > max_twist_mag_)
+      twist_ *= (max_twist_mag_ / twist_norm);
 
     // Deadzone near goal to stop tiny jitter and reset integral
     if (error_norm < min_pos_error) {
@@ -176,13 +189,19 @@ private:
       goal_crossed_ = false;
     }
 
-    cmd_vel_msg_.linear.x = twist_(0);
-    cmd_vel_msg_.linear.y = twist_(1);
-    cmd_vel_publisher_->publish(cmd_vel_msg_);
+    float yaw_error = odom_init_yaw_ - odom_yaw_;
 
     data_msg_.data[0] = error_norm;
     data_msg_.data[1] = odom_vel_.norm();
+    data_msg_.data[2] = integral_.norm();
+    data_msg_.data[3] = twist_.norm();
+    data_msg_.data[4] = yaw_error;
     data_publisher_->publish(data_msg_);
+
+    cmd_vel_msg_.linear.x = twist_(0);
+    cmd_vel_msg_.linear.y = twist_(1);
+    cmd_vel_msg_.angular.z = kp_ang_ * yaw_error;
+    cmd_vel_publisher_->publish(cmd_vel_msg_);
   }
 };
 
